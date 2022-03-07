f0bef449e347bf3011b74272ded5d9c3218ee46d
Fahrul Fauzi <fahrul.fauz@gmail.com>
2022-03-07 23:41:15 +0700

    feat(middleware) : authentication middleware

---
+++ auth/middlewares/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = async (req, res, next) => {
+    if (!req.headers["authorization"]) {
+        return res.sendError({
+            Authorization: "Invalid Token!"
+        }, "Invalid Token!", 401)
+    }
+
+    const token = req.headers["authorization"].split(" ")
+    try {
+        let decoded = jwt.verify(token[1], "secret")
+
+        req.user = decoded
+        return next() // use next to go next router
+
+    } catch(err) {
+        return res.sendError(err, "Invalid Token!", 401)
+    }
+}
