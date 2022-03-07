57d9b7c9b5db7c6a8816c2dd02b3caf7154901e1
Fahrul Fauzi <fahrul.fauz@gmail.com>
2022-03-07 22:38:01 +0700

    feat(validator) : remove validator email

--- auth/lib/validator.js
+++ auth/lib/validator.js
@@ -1,14 +1,11 @@
 module.exports = {
     checkBody: function (reqBody, requires, strings) {
         const required = "$_variable is required"
-        const emailInvalid = "Email is invalid"
 
         let errors = {}
         for (let i in requires) {
             if (typeof reqBody[requires[i]] === 'undefined' || (reqBody[requires[i]] === "")) {
                 errors[requires[i]] = required.replace('$_variable', requires[i])
-            } else if (requires[i] === 'email' && !validateEmail(reqBody[requires[i]])) {
-                errors[requires[i]] = emailInvalid
             }
         }
 
@@ -17,9 +14,4 @@ module.exports = {
         }
         return false
     }
-}
-
-function validateEmail(email) {
-    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
 }
